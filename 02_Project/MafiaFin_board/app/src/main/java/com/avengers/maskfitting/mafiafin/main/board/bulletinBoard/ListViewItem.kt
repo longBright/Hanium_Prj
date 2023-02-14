8a85f0d3088226ff3f697673bf1b44ec15171f64
youhyeon <rhrnak0519@naver.com>
2023-02-14 22:12:58 +0900

    23.02.14 Hwang

--- 02_Project/MafiaFin_board/app/src/main/java/com/avengers/maskfitting/mafiafin/main/board/bulletinBoard/ListViewItem.kt
+++
@@ -1,8 +0,0 @@
-package com.avengers.maskfitting.mafiafin.main.board.bulletinBoard
-
-import android.graphics.drawable.Drawable
-
-class ListViewItem {
-    var titleStr: String? = null
-    var contentStr: String? = null
-}
--- 02_Project/MafiaFin_board/app/src/main/java/com/avengers/maskfitting/mafiafin/main/weather/model/nearbycenter/StationResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.avengers.maskfitting.mafiafin.main.weather.model.nearbycenter
-
-
-import com.google.gson.annotations.SerializedName
-
-data class StationResponse(
-    @SerializedName("response")
-    val response: Response?
-)
